2493967f541a9f1c7751511a64c779fb472d3b58
Bin Qian <bin.qian@windriver.com>
2018-06-29 14:09:55 -0400

    create independent sm-api client 1. create smapi service endpoint, credential 2. create sm-api.conf for sm-api configuration 3. configure haproxy, so sm-api binds to mgmt fixed ip, with haproxy     mgmt fixed ip -> mgmt floating ip -> oam floating ip
    Change-Id: I743f6b0f09c06325d3c9dc6e11902420882e7931
    Signed-off-by: Bin Qian <bin.qian@windriver.com>

---
+++ puppet-modules-wrs/puppet-smapi/src/smapi/manifests/keystone/auth.pp
@@ -0,0 +1,50 @@
+#
+# Files in this package are licensed under Apache; see LICENSE file.
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# The right to copy, distribute, modify, or otherwise make use
+# of this software may be licensed only pursuant to the terms
+# of an applicable Wind River license agreement.
+#
+
+
+# == Class: smapi::keystone::auth
+#
+# Configures smapi user, service and endpoint in Keystone.
+#
+
+class smapi::keystone::auth (
+  $configure_endpoint   = true,
+  $configure_user       = true,
+  $configure_user_role  = true,
+  $password             = 'passwd',
+  $auth_name            = 'smapi',
+  $public_url           = 'http://127.0.0.1:7777',
+  $admin_url            = 'http://127.0.0.1:7777',
+  $internal_url         = 'http://127.0.0.1:7777',
+  $tenant               = 'services',
+  $region               = 'RegionOne',
+  $service_description  = 'sm-api service',
+  $service_name         = 'smapi',
+  $service_type         = 'smapi',
+) {
+
+  $real_service_name = pick($service_name, $auth_name)
+
+  keystone::resource::service_identity { $auth_name:
+    configure_endpoint  => $configure_endpoint,
+    configure_user      => $configure_user,
+    configure_user_role => $configure_user_role,
+    password            => $password,
+    auth_name           => $auth_name,
+    public_url          => $public_url,
+    admin_url           => $admin_url,
+    internal_url        => $internal_url,
+    tenant              => $tenant,
+    region              => $region,
+    service_description => $service_description,
+    service_name        => $real_service_name,
+    service_type        => $service_type,
+  }
+}
