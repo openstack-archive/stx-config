0110d264357553793e67a4a5ac001c2bb76cd11c
Teresa Ho <teresa.ho@windriver.com>
2018-07-10 10:37:54 -0400

    Extend sysinv to assign kubernetes labels to nodes
    Add CLI commands and restapi to assign and remove kubernetes labels to
    hosts.
    
    Change-Id: I3f68fcd331d5539429f86dfd4bf7514dc963bedb
    Signed-off-by: David Sullivan <david.sullivan@windriver.com>
    Story: 2002845
    Task: 22793
    Depends-On: I514ada0563ac16dd77a33979a4c021e86c59ada9

---
+++ sysinv/cgts-client/cgts-client/cgtsclient/v1/label.py
@@ -0,0 +1,43 @@
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# The right to copy, distribute, modify, or otherwise make use
+# of this software may be licensed only pursuant to the terms
+# of an applicable Wind River license agreement.
+#
+
+# -*- encoding: utf-8 -*-
+#
+
+from cgtsclient.common import base
+
+
+class KubernetesLabel(base.Resource):
+    def __repr__(self):
+        return "<KubernetesLabel %s>" % self._info
+
+
+class KubernetesLabelManager(base.Manager):
+    resource_class = KubernetesLabel
+
+    @staticmethod
+    def _path(label_id=None):
+        return '/v1/labels/%s' % label_id if label_id else \
+            '/v1/labels'
+
+    def list(self, ihost_id):
+        path = '/v1/ihosts/%s/labels' % ihost_id
+        return self._list(path, "labels")
+
+    def get(self, label_id):
+        path = '/v1/labels/%s' % label_id
+        try:
+            return self._list(path)[0]
+        except IndexError:
+            return None
+
+    def assign(self, host_uuid, label):
+        return self._create(self._path(host_uuid), label)
+
+    def remove(self, uuid):
+        return self._delete(self._path(uuid))
