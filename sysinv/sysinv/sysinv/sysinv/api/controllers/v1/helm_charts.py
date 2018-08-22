3a6ec31ebc7ec2bc0933735b511dad24030f9b31
Bailey, Henry Albert (Al) <Al.Bailey@windriver.com>
2018-08-22 14:42:14 -0400

    Fix sysinv pep8 failure of type F841
    F841: local variable 'db_values' is assigned to but never used.
    
    The unused variable is located within a try block in helm_charts.py
    This line is required as the code checks to see if it throws an
    exception.
    
    A comment was added to make the intention of this line clearer.
    
    Story: 2002888
    Task: 23097
    
    Change-Id: Ia194a4d6127a5d17e572750457b83fbd034b4df7
    Signed-off-by: Mathieu Robinson <mathieu.robinson@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/api/controllers/v1/helm_charts.py
+++ sysinv/sysinv/sysinv/sysinv/api/controllers/v1/helm_charts.py
@@ -87,7 +87,9 @@ class HelmChartsController(rest.RestController):
         try:
             db_chart = objects.helm_overrides.get_by_name(
                 pecan.request.context, name, namespace)
-            db_values = db_chart.user_overrides
+            # The following line is necessary as we are checking to
+            # see if it throws an exception
+            db_values = db_chart.user_overrides  # noqa
         except exception.HelmOverrideNotFound:
             if name in SYSTEM_CHARTS:
                 pecan.request.dbapi.helm_override_create({
