e1705bd8c4d9caefe5e0224742fb914c5e4d1dea
Steven Webster <steven.webster@windriver.com>
2018-09-18 14:40:20 -0400

    LLDP OVS enablement: puppet configuration
    This commit introduces puppet configuration enabling LLDP to operate over
    OVS.  Specifically, separate ports, bridges and flows are configured to
    handle LLDP traffic.  LLDP traffic is forwarded to a separate bridge to
    prevent the neutron ovs agent from deleting stx-config flows on
    restart and/or init.  In addition, we restrict the lldpd daemon from
    operating over bridge, tap, and ovs-netdev devices.
    
    Story: 2002946
    Task: 22940
    
    Change-Id: Ibadc9c082425412b5b68b02a55e8c02692de0e17
    Signed-off-by: Steven Webster <steven.webster@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/puppet/platform.py
+++ sysinv/sysinv/sysinv/sysinv/puppet/platform.py
@@ -70,6 +70,7 @@ class PlatformPuppet(base.BasePuppet):
         config.update(self._get_host_tpm_config(host))
         config.update(self._get_host_cpu_config(host))
         config.update(self._get_host_memory_config(host))
+        config.update(self._get_host_lldp_config(host))
         return config
 
     def _get_static_software_config(self):
@@ -818,3 +819,17 @@ class PlatformPuppet(base.BasePuppet):
     def _get_platform_cpu_count(self, host):
         cpus = self._get_host_cpu_list(host, constants.PLATFORM_FUNCTION, True)
         return len(cpus)
+
+    def _get_host_lldp_config(self, host):
+        driver_list = []
+
+        # Default is lldpd
+        driver_list.append('lldpd')
+
+        if (constants.COMPUTE in utils.get_personalities(host) and
+                self._vswitch_type() == constants.VSWITCH_TYPE_OVS_DPDK):
+            driver_list.append('ovs')
+
+        return {
+            'sysinv::agent::lldp_drivers': driver_list
+        }
