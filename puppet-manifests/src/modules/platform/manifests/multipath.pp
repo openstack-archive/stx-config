4a03aa7ec6c860b45f0da031969736b773471060
Daniel Badea <daniel.badea@windriver.com>
2018-10-03 20:50:53 +0000

    iscsi multipath support
    Add service parameter to enable multiple iSCSI multipath:
    
       system service-parameter-add cinder DEFAULT multipath=true
       system service-parameter-apply cinder
    
    Add Puppet manifests for multipathd service.
    
    When multipath service parameter is enabled then controller
    and compute nodes go into config out-of-date status. They
    need to be locked/unlocked to apply the new configuration:
    
    1. cinder.conf (only for controllers)
    
           [DEFAULT]
           use_multipath_for_image_xfer=true
    
           [backend_defaults]
           use_multipath_for_image_xfer=true
    
       Note:
         a. enforce_multipath_for_image_xfer option is not set
            to allow fallback to single path in case multipathd
            service is not available
    
    2.  load dm_multipath kernel module
    
    3.  multipath.conf
          defaults {
              user_friendly_names yes
              find_multipaths yes
          }
    
          blacklist {
              device {
                  vendor "*"
              }
          }
    
          blacklist_exceptions {
              property "(SCSI_IDENT_|ID_WWN)"
              device {
                  vendor "3PARdata"
              }
          }
    
        Notes:
          a. multipath support is enabled only for hpe3par
             iSCSI devices by:
             - blacklisting devices from all vendors
             - blacklisting all devices that do not
               have ID_WWN or SCSI_IDENT_ udev entries
               (see multipath.conf manual page)
             - making an exception for 3PARdata vendor
    
          b. multipath service is managed by systemd
    
    Story: 2002996
    Task: 27040
    Change-Id: Ib34d4542f93e78a507073f43808658be2b9fc87d
    Signed-off-by: Daniel Badea <daniel.badea@windriver.com>

---
+++ puppet-manifests/src/modules/platform/manifests/multipath.pp
@@ -0,0 +1,45 @@
+class platform::multipath::params (
+  $enabled = false,
+) {
+}
+
+class platform::multipath
+  inherits platform::multipath::params {
+  if $enabled {
+    file { '/etc/multipath.conf':
+      ensure  => 'present',
+      mode    => '0644',
+      content => template("platform/multipath.conf.erb")
+    } ->
+    service { 'start-multipathd':
+      ensure     => 'running',
+      enable     => true,
+      name       => 'multipathd',
+      hasstatus  => true,
+      hasrestart => true,
+    } ->
+    exec { 'systemctl-enable-multipathd':
+      command => '/usr/bin/systemctl enable multipathd.service',
+    } ->
+    exec { 'systemctl-start-multipathd':
+      command => '/usr/bin/systemctl start multipathd.service',
+    }
+  } else {
+    service { 'stop-multipathd':
+      ensure     => 'stopped',
+      enable     => false,
+      name       => 'multipathd',
+      hasstatus  => true,
+      hasrestart => true,
+    } ->
+    exec { 'systemctl-stop-multipathd':
+      command => '/usr/bin/systemctl stop multipathd.service',
+    } ->
+    exec { 'systemctl-disable-multipathd':
+      command => '/usr/bin/systemctl disable multipathd.service',
+    } ->
+    file { '/etc/multipath.conf':
+      ensure  => 'absent',
+    }
+  }
+}
