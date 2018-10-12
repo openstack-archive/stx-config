95bc3619fc2f620177e57e07f049b47a11d681ac
zhangyangyang <zhangyangyang@unionpay.com>
2018-10-12 17:13:24 +0800

    Fix basestring issue for Python 2/3 compatible code.
    replace basestring with six.string_types
    need python-six module
    
    Story: 2003433
    Task: 27093
    
    Change-Id: I6b7fac3910279cd48cda9be382d64f841638d1da
    Signed-off-by: zhangyangyang <zhangyangyang@unionpay.com>

--- sysinv/sysinv/sysinv/sysinv/openstack/common/jsonutils.py
+++ sysinv/sysinv/sysinv/sysinv/openstack/common/jsonutils.py
@@ -52,7 +52,7 @@ _nasty_type_tests = [inspect.ismodule, inspect.isclass, inspect.ismethod,
                      inspect.iscode, inspect.isbuiltin, inspect.isroutine,
                      inspect.isabstract]
 
-_simple_types = (types.NoneType, int, basestring, bool, float, long)
+_simple_types = (types.NoneType, int, six.string_types, bool, float, long)
 
 
 def to_primitive(value, convert_instances=False, convert_datetime=True,
