e13ff65277c9b04b7cd46f2ae6d95828cfc5efd6
Irina Mihai <irina.mihai@windriver.com>
2018-10-19 20:03:09 +0000

    Updates to the existing Glance overrides
    Add overrides support for:
      - docker image source
      - replicas based on number of controllers
      - user passwords
      - storage based on provisioned backend
    
    Change-Id: I02a2bede1462b0925a124d64dcff4cf35cc48d48
    Story: 2003909
    Task: 27084
    Co-Authored-By: Ovidiu Poncea <ovidiu.poncea@windriver.com>
    Co-Authored-By: Robert Church <robert.church@windriver.com>
    Signed-off-by: Irina Mihai <irina.mihai@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/helm/glance.py
+++ sysinv/sysinv/sysinv/sysinv/helm/glance.py
@@ -6,11 +6,16 @@
 
 from sysinv.common import constants
 from sysinv.common import exception
+from sysinv.common.storage_backend_conf import StorageBackendConfig
 from sysinv.openstack.common import log as logging
+
 from . import common
 from . import openstack
 
 LOG = logging.getLogger(__name__)
+
+# Info used in the Glance Helm chart.
+RBD_STORE_USER = 'images'
 
 
 class GlanceHelm(openstack.OpenstackBaseHelm):
@@ -21,13 +26,28 @@ class GlanceHelm(openstack.OpenstackBaseHelm):
         common.HELM_NS_OPENSTACK
     ]
 
+    SERVICE_NAME = 'glance'
+    AUTH_USERS = ['glance']
+
+    @property
+    def docker_repo_source(self):
+        return common.DOCKER_SRC_STX
+
+    @property
+    def docker_repo_tag(self):
+        return common.DOCKER_SRCS[self.docker_repo_source][common.IMG_TAG_KEY]
+
     def get_namespaces(self):
         return self.SUPPORTED_NAMESPACES
 
     def get_overrides(self, namespace=None):
         overrides = {
             common.HELM_NS_OPENSTACK: {
-                'storage': 'pvc'
+                'pod': self._get_pod_overrides(),
+                'endpoints': self._get_endpoints_overrides(),
+                'storage': self._get_storage_overrides(),
+                'conf': self._get_conf_overrides(),
+                'images': self._get_images_overrides(),
             }
         }
 
@@ -38,3 +58,112 @@ class GlanceHelm(openstack.OpenstackBaseHelm):
                                                  namespace=namespace)
         else:
             return overrides
+
+    def _get_images_overrides(self):
+        heat_image = self._operator.chart_operators[
+            constants.HELM_CHART_HEAT].docker_image
+        return {
+            'tags': {
+                'bootstrap': heat_image,
+                'db_drop': heat_image,
+                'db_init': heat_image,
+                'glance_api': self.docker_image,
+                'glance_db_sync': self.docker_image,
+                'glance_registry': self.docker_image,
+                'ks_endpoints': heat_image,
+                'ks_service': heat_image,
+                'ks_user': heat_image,
+            }
+        }
+
+    def _get_pod_overrides(self):
+        return {
+            'replicas': {
+                'api': 1,
+                'registry': 1,
+            }
+        }
+
+    def _get_endpoints_identity_users_overrides(self):
+        overrides = {}
+        overrides.update(self._get_common_users_overrides(self.SERVICE_NAME))
+
+        for user in self.AUTH_USERS:
+            overrides.update({
+                user: {
+                    'region_name': self._region_name(),
+                    'password': self._get_keyring_password(self.SERVICE_NAME,
+                                                           user)
+                }
+            })
+        return overrides
+
+    def _get_endpoints_identity_overrides(self):
+        return {'auth': self._get_endpoints_identity_users_overrides()}
+
+    def _get_endpoints_overrides(self):
+        return {
+            'identity': self._get_endpoints_identity_overrides(),
+        }
+
+    def _get_storage_overrides(self):
+        ceph_backend = self._get_primary_ceph_backend()
+
+        if not ceph_backend:
+            return 'pvc'
+
+        return 'rbd'  # radosgw| rbd | swift | pvc
+
+    def _get_ceph_overrides(self):
+        # Get Ceph monitors.
+        ceph_mon_ips = StorageBackendConfig.get_ceph_mon_ip_addresses(
+            self.dbapi).values()
+        conf_ceph = {
+            'admin_keyring': self._get_ceph_password(
+                self.SERVICE_NAME, 'admin_keyring'
+            ),
+            'monitors': ["%s:6789" % ceph_mon_ips[0],
+                         "%s:6789" % ceph_mon_ips[1],
+                         "%s:6789" % ceph_mon_ips[2]]
+        }
+
+        return conf_ceph
+
+    def _get_conf_overrides(self):
+        ceph_backend = self._get_primary_ceph_backend()
+        if not ceph_backend:
+            rbd_store_pool = ""
+            rbd_store_user = ""
+        else:
+            rbd_store_pool = constants.CEPH_POOL_IMAGES_NAME
+            rbd_store_user = RBD_STORE_USER
+
+        conf = {
+            'glance': {
+                'DEFAULT': {
+                    'graceful_shutdown': True,
+                    'show_image_direct_url': True,
+                    'workers': 6
+                },
+                'glance_store': {
+                    'filesystem_store_datadir': constants.GLANCE_IMAGE_PATH,
+                    'rbd_store_pool': rbd_store_pool,
+                    'rbd_store_user': rbd_store_user,
+                }
+            }
+        }
+
+        if ceph_backend:
+            conf['ceph'] = self._get_ceph_overrides()
+
+        return conf
+
+    def _get_primary_ceph_backend(self):
+        try:
+            backend = self.dbapi.storage_backend_get_by_name(
+                constants.SB_DEFAULT_NAMES[constants.SB_TYPE_CEPH])
+        except Exception:
+            backend = None
+            pass
+
+        return backend
