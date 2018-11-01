2f19417f9ba36b0c7d356209840d5e43ca3eef53
Kevin Smith <kevin.smith@windriver.com>
2018-11-01 15:38:20 -0400

    Kubernetes, VIM label support
    Changes include:
    -Sysinv will inform VIM when Kubernetes labels change.
    -Prep Sysinv semantic checks on label addition.
    
    Change-Id: If1f81eafc50af3dd310d7592dab56de0dc102f02
    Story: 2002843
    Task: 27752
    Signed-off-by: Kevin Smith <kevin.smith@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/api/controllers/v1/label.py
+++ sysinv/sysinv/sysinv/sysinv/api/controllers/v1/label.py
@@ -12,6 +12,8 @@ from sysinv.api.controllers.v1 import base
 from sysinv.api.controllers.v1 import collection
 from sysinv.api.controllers.v1 import types
 from sysinv.api.controllers.v1 import utils
+from sysinv.api.controllers.v1 import vim_api
+from sysinv.common import constants
 from sysinv.common import exception
 from sysinv.common import utils as cutils
 from sysinv.openstack.common import excutils
@@ -106,6 +108,7 @@ class LabelController(rest.RestController):
     }
 
     def __init__(self, from_ihosts=False):
+        self._api_token = None
         self._from_ihosts = from_ihosts
 
     def _get_labels_collection(self, host_uuid, marker, limit, sort_key,
@@ -196,6 +199,8 @@ class LabelController(rest.RestController):
         LOG.info("patch_data: %s" % body)
         host = objects.host.get_by_uuid(pecan.request.context, uuid)
 
+        _semantic_checks(host)
+
         try:
             pecan.request.rpcapi.update_kubernetes_label(
                 pecan.request.context,
@@ -215,11 +220,23 @@ class LabelController(rest.RestController):
                 'label_key': key,
                 'label_value': value
             }
+
             try:
                 new_label = pecan.request.dbapi.label_create(uuid, values)
                 new_records.append(new_label)
             except exception.HostLabelAlreadyExists:
                 pass
+
+        try:
+            vim_api.vim_host_update(
+                self._api_token,
+                uuid,
+                host.hostname,
+                constants.VIM_DEFAULT_TIMEOUT_IN_SECS)
+        except Exception as e:
+            LOG.warn(_("No response vim_api host:%s e=%s" %
+                     (host.hostname, e)))
+            pass
 
         return LabelCollection.convert_with_links(
             new_records, limit=None, url=None, expand=False,
@@ -235,6 +252,8 @@ class LabelController(rest.RestController):
         lbl_obj = objects.label.get_by_uuid(pecan.request.context, uuid)
         host = objects.host.get_by_uuid(pecan.request.context, lbl_obj.host_id)
         label_dict = {lbl_obj.label_key: None}
+
+        _semantic_checks(host)
 
         try:
             pecan.request.rpcapi.update_kubernetes_label(
@@ -253,3 +272,26 @@ class LabelController(rest.RestController):
             msg = _("Delete host label failed: host %s label %s=%s"
                     % (host.hostname, lbl_obj.label_key, lbl_obj.label_value))
             raise wsme.exc.ClientSideError(msg)
+
+        try:
+            vim_api.vim_host_update(
+                self._api_token,
+                host.uuid,
+                host.hostname,
+                constants.VIM_DEFAULT_TIMEOUT_IN_SECS)
+        except Exception as e:
+            LOG.warn(_("No response vim_api host:%s e=%s" %
+                     (host.hostname, e)))
+            pass
+
+
+def _semantic_checks(host):
+
+    # TODO(ksmith):
+    # turn this on later
+    return
+
+    if utils.is_aio_simplex_host_unlocked(host):
+        raise wsme.exc.ClientSideError(_("Host must be locked."))
+    elif host.administrative != constants.ADMIN_LOCKED:
+        raise wsme.exc.ClientSideError(_("Host must be locked."))
