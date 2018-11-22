fb85b92135a949a3ece015efbdf7c50178274395
Wei Zhou <wei.zhou@windriver.com>
2018-11-22 18:21:02 -0500

    Enhancement for Controller Filesystem Extension
    This commit adds following enhancement for controller filesystem
    extension:
    
    - Add a new column/row to show the size of available space in the
      volume group in host-lvg-list and host-lvg-show command output.
      This will help user know how much space is available to extend
      controller filesystem.
    - Remove the 2G space reservation when calculating available space
      in cgts-vg volume group.
    - Remove the unused code when calculating available space in cgts-vg
      volume group.
    
    Change-Id: Ic94a3544a9d02f67c592e07048b95135c2b350ed
    Closes-Bug: 1804711
    Signed-off-by: Wei Zhou <wei.zhou@windriver.com>

--- sysinv/cgts-client/cgts-client/cgtsclient/common/utils.py
+++ sysinv/cgts-client/cgts-client/cgtsclient/common/utils.py
@@ -23,6 +23,7 @@ except Exception:
 import argparse
 import copy
 import dateutil
+import math
 import os
 import prettytable
 import re
@@ -749,10 +750,12 @@ def extract_keypairs(args):
     return attributes
 
 
-# Convert size from BYTE to KiB, MiB, GiB, TiB, PiB
-# 1 - KiB, 2 - MiB, 3 - GiB, 4 - TiB, 5 - PiB
-def convert_size_from_bytes(bytes, type):
-    return '%.2f' % (float(bytes) / (1024 ** type))
+# Convert size from a smaller unit (e.g. KiB) to a larger
+# unit (e.g. GiB). Each 1024 is 1 step. e.g. From BYTE to
+# MiB is 2 steps. From MiB to GiB is 1 step. The return value
+# is a float with 3 digits after the decimal point.
+def size_unit_conversion(size, step):
+    return math.floor(float(size) / (1024 ** step) * 1000) / 1000.0
 
 
 def _get_system_info(cc):
