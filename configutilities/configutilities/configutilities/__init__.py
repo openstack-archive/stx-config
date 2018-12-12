df788522c2e9af1d3bb5392ad4313af037a73579
Sun Austin <austin.sun@intel.com>
2018-12-12 23:21:29 +0800

    Fix: "import" issue for Python 2/3 compatible code
    use absolute path imports to compat python3
    remove H301 ignore to enable H304 flake8 check
    
    Story: 2003433
    Task: 28376
    
    Change-Id: I3a50a0298fe34c60e3c63df23e72dcbb07c585d1
    Signed-off-by: Sun Austin <austin.sun@intel.com>

--- configutilities/configutilities/configutilities/__init__.py
+++ configutilities/configutilities/configutilities/__init__.py
@@ -6,14 +6,28 @@
 # flake8: noqa
 #
 
-from common.validator import validate
-from common.configobjects import (Network, DEFAULT_CONFIG, REGION_CONFIG,
-                                  DEFAULT_NAMES, HP_NAMES, SUBCLOUD_CONFIG,
-                                  MGMT_TYPE, INFRA_TYPE, OAM_TYPE,
-                                  NETWORK_PREFIX_NAMES, HOST_XML_ATTRIBUTES,
-                                  DEFAULT_DOMAIN_NAME)
-from common.exceptions import ConfigError, ConfigFail, ValidateFail
-from common.utils import is_valid_vlan, is_mtu_valid, \
-    validate_network_str, validate_address_str, validate_address, \
-    ip_version_to_string, lag_mode_to_str, \
-    validate_openstack_password, extract_openstack_password_rules_from_file
+from configutilities.common.validator import validate
+from configutilities.common.configobjects import Network
+from configutilities.common.configobjects import DEFAULT_CONFIG
+from configutilities.common.configobjects import REGION_CONFIG
+from configutilities.common.configobjects import DEFAULT_NAMES
+from configutilities.common.configobjects import HP_NAMES
+from configutilities.common.configobjects import SUBCLOUD_CONFIG
+from configutilities.common.configobjects import MGMT_TYPE
+from configutilities.common.configobjects import INFRA_TYPE
+from configutilities.common.configobjects import OAM_TYPE
+from configutilities.common.configobjects import NETWORK_PREFIX_NAMES
+from configutilities.common.configobjects import HOST_XML_ATTRIBUTES
+from configutilities.common.configobjects import DEFAULT_DOMAIN_NAME
+from configutilities.common.exceptions import ConfigError
+from configutilities.common.exceptions import ConfigFail
+from configutilities.common.exceptions import ValidateFail
+from configutilities.common.utils import is_valid_vlan
+from configutilities.common.utils import is_mtu_valid
+from configutilities.common.utils import validate_network_str
+from configutilities.common.utils import validate_address_str
+from configutilities.common.utils import validate_address
+from configutilities.common.utils import ip_version_to_string
+from configutilities.common.utils import lag_mode_to_str
+from configutilities.common.utils import validate_openstack_password
+from configutilities.common.utils import extract_openstack_password_rules_from_file
