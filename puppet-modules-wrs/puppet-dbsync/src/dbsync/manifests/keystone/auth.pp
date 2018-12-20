b3a0e207d19b9170bf593f490b8f799c39883ca8
Andy Ning <andy.ning@windriver.com>
2018-12-20 11:22:14 -0500

    Keystone DB record synchronization
    This update adds the puppet package for keystone DB synchronizatin
    service. This puppet package will be used by controller puppet manifest
    to deploy and configure the synchronization service.
    
    Story: 2002842
    Task: 22787
    
    Change-Id: Ib55f3be1c85dfdb7d8bf972a8a8b549c583f4b43
    Signed-off-by: Andy Ning <andy.ning@windriver.com>

---
+++ puppet-modules-wrs/puppet-dbsync/src/dbsync/manifests/keystone/auth.pp
@@ -0,0 +1,51 @@
+#
+# Files in this package are licensed under Apache; see LICENSE file.
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#  Jan 2019: creation
+#
+
+# == Class: dbsync::keystone::auth
+#
+# Configures dbsync user, service and endpoint in Keystone.
+#
+class dbsync::keystone::auth (
+  $password,
+  $auth_domain,
+  $auth_name            = 'dbsync',
+  $email                = 'dbsync@localhost',
+  $tenant               = 'services',
+  $region               = 'RegionOne',
+  $service_description  = 'DCOrch dbsync service',
+  $service_name         = 'dbsync',
+  $service_type         = 'dcorch-dbsync',
+  $configure_endpoint   = true,
+  $configure_user       = true,
+  $configure_user_role  = true,
+  $public_url           = 'http://127.0.0.1:8219/v1',
+  $admin_url            = 'http://127.0.0.1:8219/v1',
+  $internal_url         = 'http://127.0.0.1:8219/v1',
+) {
+
+  $real_service_name = pick($service_name, $auth_name)
+
+  keystone::resource::service_identity { 'dbsync':
+    configure_user      => $configure_user,
+    configure_user_role => $configure_user_role,
+    configure_endpoint  => $configure_endpoint,
+    service_type        => $service_type,
+    service_description => $service_description,
+    service_name        => $real_service_name,
+    region              => $region,
+    auth_name           => $auth_name,
+    password            => $password,
+    email               => $email,
+    tenant              => $tenant,
+    public_url          => $public_url,
+    admin_url           => $admin_url,
+    internal_url        => $internal_url,
+  }
+}
---
+++ puppet-modules-wrs/puppet-dbsync/src/dbsync/manifests/params.pp
@@ -0,0 +1,29 @@
+#
+# Files in this package are licensed under Apache; see LICENSE file.
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+
+class dbsync::params {
+
+  $dbsync_dir = '/etc/dbsync'
+  $dbsync_conf = '/etc/dbsync/dbsync.conf'
+
+  if $::osfamily == 'Debian' {
+    $package_name       = 'distributedcloud-dbsync'
+    $api_package        = 'distributedcloud-dbsync'
+    $api_service        = 'dbsync-api'
+
+  } elsif($::osfamily == 'RedHat') {
+
+    $package_name       = 'distributedcloud-dbsync'
+    $api_package        = false
+    $api_service        = 'dbsync-api'
+
+  } else {
+    fail("unsuported osfamily ${::osfamily}, currently Debian, Redhat are the only supported platforms")
+  }
+}
