f5858024c340731fc816e5dc527c2d48004435b9
Alex Kozyrev <alex.kozyrev@windriver.com>
2019-01-18 16:04:13 -0500

    Store BMC password in Openstack Barbican instead of keyring.
    Replacing existing mechanism of storing BMC passwords in SysInv.
    Implementing access to Barbican API in SysInv and using it to write
    the passwords into a Barbican secrets. Note that a Barbican cannot
    change the existing password inside its secret, so we need to remove
    the old secret and create a new one in case of password update.
    Another thing to mention: SysInv has to create Barbican secrets in
    context of "services" project in order MTCE can read them later.
    
    Change-Id: I7102a9662f3757c062ab310737f4ba08379d0100
    Story: 2003108
    Task: 27700
    Signed-off-by: Alex Kozyrev <alex.kozyrev@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/conductor/manager.py
+++ sysinv/sysinv/sysinv/sysinv/conductor/manager.py
@@ -9359,38 +9359,23 @@ class ConductorManager(service.PeriodicService):
         target_load = self.dbapi.load_get(host_upgrade.target_load)
         return target_load.software_version == tsc.SW_VERSION
 
-    def configure_keystore_account(self, context, service_name,
-                                   username, password):
-        """Synchronously, have a conductor configure a ks(keyring) account.
-
-        Does the following tasks:
-        - call keyring API to create an account under a service.
+    def create_barbican_secret(self, context, name, payload):
+        """Calls Barbican API to create a secret
 
         :param context: request context.
-        :param service_name: the keystore service.
-        :param username: account username
-        :param password: account password
+        :param name: secret name
+        :param payload: secret payload
         """
-        if (not service_name.strip()):
-            raise exception.SysinvException(_(
-                "Keystore service is a blank value"))
+        self._openstack.create_barbican_secret(context=context,
+                                               name=name, payload=payload)
 
-        keyring.set_password(service_name, username, password)
-
-    def unconfigure_keystore_account(self, context, service_name, username):
-        """Synchronously, have a conductor unconfigure a ks(keyring) account.
-
-        Does the following tasks:
-        - call keyring API to delete an account under a service.
+    def delete_barbican_secret(self, context, name):
+        """Calls Barbican API to delete a secret
 
         :param context: request context.
-        :param service_name: the keystore service.
-        :param username: account username
+        :param name: secret name
         """
-        try:
-            keyring.delete_password(service_name, username)
-        except keyring.errors.PasswordDeleteError:
-            pass
+        self._openstack.delete_barbican_secret(context=context, name=name)
 
     def update_snmp_config(self, context):
         """Update the snmpd configuration"""
