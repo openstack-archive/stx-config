ad174704d8de11ff52718bff24cd0ad270a8c889
Mingyuan Qi <mingyuan.qi@intel.com>
2019-01-30 09:41:27 +0800

    Allow user specified registries for config_controller
    Currently docker images were pulled from public registries during
    config_controller. For some users, the connection to the public
    docker registry may be slow such that installing the containerized
    services images may timeout or the system simply does not have
    access to the public internet.
    
    This change allows users to specify alternative public/private
    registries to replace k8s.gcr.io, gcr.io, quay.io and docker.io.
    Insecure registry is supported if all default registries were
    replaced by one unified registry. It lowers the complexity for
    those who build his own registry without internet access.
    
    Test:
    AIO-SX/AIO-DX/Standard(2+2):
      Alternative public registry (ipv4/domain) with proxy
        - config_controller pass
      Private registry (ipv4/domain) without internet
        - config_controller pass
      Default registry with/without proxy
        - config_controller pass
    
    Story: 2004711
    Task: 28742
    
    Change-Id: I4fee3f4e0637863b9b5ef4ef556082ac75f62a1d
    Signed-off-by: Mingyuan Qi <mingyuan.qi@intel.com>

--- puppet-manifests/src/modules/platform/manifests/kubernetes.pp
+++ puppet-manifests/src/modules/platform/manifests/kubernetes.pp
@@ -13,8 +13,26 @@ class platform::kubernetes::params (
 ) { }
 
 class platform::kubernetes::kubeadm {
+  include ::platform::docker::params
+
   $iptables_file = "net.bridge.bridge-nf-call-ip6tables = 1
     net.bridge.bridge-nf-call-iptables = 1"
+
+  if $::platform::docker::params::k8s_registry {
+    $k8s_registry = $::platform::docker::params::k8s_registry
+  } else {
+    $k8s_registry = undef
+  }
+
+  # kubelet use --pod-infra-container-image to indentify the specified image
+  # TODO: this is not needed after kubernetes upgraded to 1.13
+  #       because the imageRepository setting will be used
+  if $k8s_registry {
+    file { '/etc/sysconfig/kubelet':
+      ensure  => file,
+      content => template('platform/kubelet.conf.erb'),
+    }
+  }
 
   # Update iptables config. This is required based on:
   # https://kubernetes.io/docs/tasks/tools/install-kubeadm
@@ -52,6 +70,21 @@ class platform::kubernetes::master::init
   inherits ::platform::kubernetes::params {
 
   include ::platform::params
+  include ::platform::docker::params
+
+  # This is used for imageRepository in template kubeadm.yaml.erb
+  if $::platform::docker::params::k8s_registry {
+    $k8s_registry = $::platform::docker::params::k8s_registry
+  } else {
+    $k8s_registry = undef
+  }
+
+  # This is used for calico image in template calico.yaml.erb
+  if $::platform::docker::params::quay_registry {
+    $quay_registry = $::platform::docker::params::quay_registry
+  } else {
+    $quay_registry = 'quay.io'
+  }
 
   if str2bool($::is_initial_config_primary) {
     # For initial controller install, configure kubernetes from scratch.
