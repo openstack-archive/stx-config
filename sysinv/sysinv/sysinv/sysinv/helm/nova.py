533affd711a2579d663bed25d7bb97338af8d81e
David Sullivan <david.sullivan@windriver.com>
2019-02-08 16:02:12 -0500

    Platform container anti-affinity settings
    Add anti-affinity settings to platform services.
    
    Change-Id: I8091a54cab98ff295eba6e7dd6fa76827d149b5f
    Story: 2004520
    Task: 29418
    Signed-off-by: David Sullivan <david.sullivan@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/helm/nova.py
+++ sysinv/sysinv/sysinv/sysinv/helm/nova.py
@@ -64,7 +64,7 @@ class NovaHelm(openstack.OpenstackBaseHelm):
                         'conductor': self._num_controllers(),
                         'consoleauth': self._num_controllers(),
                         'scheduler': self._num_controllers(),
-                        # set replicas for novncproxy once it's validated.
+                        'novncproxy': self._num_controllers()
                     },
                     'user': {
                         'nova': {
