297b4853daa19e1c944c6272ddd2f532e71aa7fa
SidneyAn <ran1.an@intel.com>
2019-02-18 22:03:57 +0800

    retry func iconfig_update_file when host peronality is none
    when we run "system dns-modify" command, the command will response after
    sysinv-db was updated, and file "/etc/resolv.conf" will be updated
    asynchronously by another process "sysinv-agent". Once the attr
    "_ihost_personality" of agent is none(initial value), it will not update
    file "/etc/resolv.conf" and will not inform sysinv client also,
    which will lead command dns-modify failed silently.
    
    This patch will retry function iconfig_update_file by which sysinv-agent
    update file "/etc/resolv.conf" when attr "_ihost_personality" is none.
    
    Closes-bug: 1812269
    
    Change-Id: I3a0437750a53607c04932c1b9b818e83903bb28b
    Signed-off-by: SidneyAn <ran1.an@intel.com>

--- sysinv/sysinv/sysinv/sysinv/common/exception.py
+++ sysinv/sysinv/sysinv/sysinv/common/exception.py
@@ -1298,6 +1298,12 @@ class InvalidHelmNamespace(Invalid):
     message = _("Invalid helm overrides namespace (%(namespace)s) for chart %(chart)s.")
 
 
+class LocalManagementPersonalityNone(Invalid):
+    message = _("Local management personality is none: "
+                "config_uuid=%(config_uuid)s, config_dict=%(config_dict)s, "
+                "host_personality=%(host_personality)s")
+
+
 class LocalManagementIpNotFound(NotFound):
     message = _("Local management IP not found: "
                 "config_uuid=%(config_uuid)s, config_dict=%(config_dict)s, "
