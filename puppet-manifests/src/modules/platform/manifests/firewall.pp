0726bf73a1b436009f365add5460891f08e6d83d
Yi Wang <yi.c.wang@intel.com>
2019-03-28 21:31:23 +0800

    [WIP] Create Calico host endpoint and policies
    * Create Calico host endpoints and policies for host protection.
      Only expose some ports on OAM for platform services, and block
      other incoming traffic.
    
    Planned tests:
    * Fresh build
    * Deployment(simplex, duplex, multi-node)
    * System-level tests (TBD)
    
    Story: 2005066
    Task: 29634
    
    Change-Id: Ibe012583225a15e7d3881cc2d520eadfc170a75c
    Signed-off-by: Yi Wang <yi.c.wang@intel.com>

--- puppet-manifests/src/modules/platform/manifests/firewall.pp
+++ puppet-manifests/src/modules/platform/manifests/firewall.pp
@@ -279,6 +279,273 @@ class platform::firewall::custom (
   }
 }
 
+define platform::firewall::calico::rule (
+  String $svc_name,
+  String $svc_proto,
+  Variant[Array, Undef] $svc_ports = undef,
+  Integer $svc_order = 100,
+) {
+  include ::platform::network::oam::params
+
+  $file_name = "/tmp/gnp_oam_${svc_name}.yaml"
+  $ip_version = $::platform::network::oam::params::subnet_version
+
+  $t_svc_name = $svc_name
+  $t_svc_order = $svc_order
+
+  if $svc_ports != undef {
+    $t_ip_version = $ip_version
+    $t_svc_ports = $svc_ports
+    $t_svc_proto = $svc_proto
+  }
+  else {
+    $t_ip_version = undef
+    $t_svc_ports = undef
+    case $svc_proto {
+      'ICMP': {
+        $t_svc_proto = $ip_version ? {
+          6 => 'ICMPv6',
+          default => 'ICMP'
+        }
+      }
+      'IGMP': {
+        $t_svc_proto = 2
+      }
+      default: {
+        err('Service proto is not supported!')
+      }
+    }
+  }
+
+  notice("t_svc_name: ${t_svc_name}")
+  notice("t_ip_version: ${t_ip_version}")
+  notice("t_svc_ports: ${t_svc_ports}")
+  notice("t_svc_proto: ${t_svc_proto}")
+  file { $file_name:
+      ensure  => file,
+      content => template('platform/calico_oam_svc_gnp.yaml.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+  }
+  -> exec { "apply resource ${file_name}":
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => "kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f ${file_name}",
+  }
+}
+
+class platform::firewall::calico::oam::services {
+  # sm
+  platform::firewall::calico::rule { 'global network policy for sm':
+    svc_name  => 'sm',
+    svc_proto => 'UDP',
+    svc_ports => [2222, 2223],
+  }
+
+  # ssh
+  platform::firewall::calico::rule { 'global network policy for ssh':
+    svc_name  => 'ssh',
+    svc_proto => 'TCP',
+    svc_ports => [22],
+  }
+
+  # icmp
+  platform::firewall::calico::rule { 'global network policy for icmp':
+    svc_name  => 'icmp',
+    svc_proto => 'ICMP',
+  }
+
+  # igmp
+  platform::firewall::calico::rule { 'global network policy for igmp':
+    svc_name  => 'igmp',
+    svc_proto => 'IGMP',
+  }
+
+  # ntp
+  platform::firewall::calico::rule { 'global network policy for ntp':
+    svc_name  => 'ntp',
+    svc_proto => 'UDP',
+    svc_ports => [123],
+  }
+
+  # snmp
+  platform::firewall::calico::rule { 'global network policy for snmp':
+    svc_name  => 'snmp',
+    svc_proto => 'UDP',
+    svc_ports => [161, 162],
+  }
+
+  # ptp
+  platform::firewall::calico::rule { 'global network policy for ptp':
+    svc_name  => 'ptp',
+    svc_proto => 'UDP',
+    svc_ports => [319, 320],
+  }
+
+  # fm
+  include ::platform::fm::params
+  notice("fm enabled: ${::platform::fm::params::service_enabled}")
+  if $::platform::fm::params::service_enabled {
+    platform::firewall::calico::rule { 'global network policy for fm-api':
+      svc_name  => 'fm',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::fm::params::api_port],
+    }
+  }
+
+  # nfv-vim
+  include ::platform::nfv::params
+  platform::firewall::calico::rule { 'global network policy for nfv-vim-api':
+      svc_name  => 'nfv-vim',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::nfv::params::api_port],
+  }
+
+  # patching
+  include ::platform::patching::params
+  platform::firewall::calico::rule { 'global network policy for patching-api':
+      svc_name  => 'patching',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::patching::params::public_port],
+  }
+
+  # sysinv
+  include ::platform::sysinv::params
+  platform::firewall::calico::rule { 'global network policy for sysinv-api':
+      svc_name  => 'sysinv',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::sysinv::params::api_port],
+  }
+
+  # smapi
+  include ::platform::smapi::params
+  platform::firewall::calico::rule { 'global network policy for sm-api':
+      svc_name  => 'sm-api',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::smapi::params::port],
+  }
+
+  # ceph
+  include ::platform::ceph::params
+  platform::firewall::calico::rule { 'global network policy for ceph-radosgw':
+      svc_name  => 'ceph-radosgw',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::ceph::params::rgw_port],
+  }
+
+  # barbican
+  include ::openstack::barbican::params
+  platform::firewall::calico::rule { 'global network policy for barbican-api':
+      svc_name  => 'barbican-api',
+      svc_proto => 'TCP',
+      svc_ports => [$::openstack::barbican::params::api_port],
+  }
+
+  # keystone
+  include ::openstack::keystone::params
+  platform::firewall::calico::rule { 'global network policy for keystone-api':
+      svc_name  => 'keystone',
+      svc_proto => 'TCP',
+      svc_ports => [$::openstack::keystone::params::api_port],
+  }
+
+  # horizon
+  include ::platform::params
+  include ::openstack::horizon::params
+  if $::platform::params::distributed_cloud_role != 'subcloud'  {
+    if $::openstack::horizon::params::enable_https {
+      $horizon_port = $::openstack::horizon::params::https_port
+    } else {
+      $horizon_port = $::openstack::horizon::params::http_port
+    }
+
+    platform::firewall::calico::rule { 'global network policy for dashboard':
+      svc_name  => 'horizon',
+      svc_proto => 'TCP',
+      svc_ports => [$horizon_port],
+    }
+  }
+
+  # dcmanager
+  include ::platform::dcmanager::params
+  if $::platform::params::distributed_cloud_role == 'systemcontroller' {
+    platform::firewall::calico::rule { 'global network policy for dcmanager-api':
+      svc_name  => 'dcmanager',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::dcmanager::params::api_port],
+    }
+  }
+
+  # dcorch
+  include ::platform::dcorch::params
+  if $::platform::params::distributed_cloud_role == 'systemcontroller' {
+    platform::firewall::calico::rule { 'global network policy for dcorch-sysinv-api-proxy':
+      svc_name  => 'dcorch-sysinv-api-proxy',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::dcorch::params::sysinv_api_proxy_port],
+    }
+    platform::firewall::calico::rule { 'global network policy for dcorch-patch-api-proxy':
+      svc_name  => 'dcorch-patch-api-proxy',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::dcorch::params::patch_api_proxy_port],
+    }
+    platform::firewall::calico::rule { 'global network policy for dcorch-identity-api-proxy':
+      svc_name  => 'dcorch-identity-api-proxy',
+      svc_proto => 'TCP',
+      svc_ports => [$::platform::dcorch::params::identity_api_proxy_port],
+    }
+  }
+}
+
+class platform::firewall::calico::oam::endpoints {
+  include ::platform::params
+  include ::platform::network::oam::params
+
+  $host = $::platform::params::hostname
+  $oam_if = $::platform::network::oam::params::interface_name
+  $oam_addr = $::platform::network::oam::params::interface_address
+
+  $t_ip_version = $::platform::network::oam::params::subnet_version
+
+  notice("host: ${host}")
+  notice("oam_if: ${oam_if}")
+  notice("oam_addr: ${oam_addr}")
+
+  # create/update oam host endpoint
+  $file_name_hep = "/tmp/hep_${host}_oam.yaml"
+  $file_name_gnp = '/tmp/gnp_outbound_oam.yaml'
+  file { $file_name_hep:
+    ensure  => file,
+    content => template('platform/calico_oam_hep.yaml.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+  }
+  -> exec { "apply resource ${file_name_hep}":
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => "kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f ${file_name_hep}",
+  }
+  -> file { $file_name_gnp:
+    ensure  => file,
+    content => template('platform/calico_oam_outbound_gnp.yaml.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+  }
+  -> exec { "apply resource ${file_name_gnp}":
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => "kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f ${file_name_gnp}",
+  }
+}
+
+class platform::firewall::calico::oam {
+  contain ::platform::firewall::calico::oam::endpoints
+  contain ::platform::firewall::calico::oam::services
+
+  Class['::platform::kubernetes::master'] -> Class[$name]
+  Class['::platform::firewall::calico::oam::endpoints']
+  -> Class['::platform::firewall::calico::oam::services']
+}
 
 class platform::firewall::oam (
   $rules_file = undef,
